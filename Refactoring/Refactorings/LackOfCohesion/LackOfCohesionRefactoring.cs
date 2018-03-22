d63420120a157cf573183b3c00cd7a990b10fc60
Joel <joel.egger@hsr.ch>
2018-03-22 16:36:40 +0100

    LackOfCohesion regognition

---
+++ Refactoring/Refactorings/LackOfCohesion/LackOfCohesionRefactoring.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Refactoring.Helper;
+
+namespace Refactoring.Refactorings.LackOfCohesion
+{
+    public sealed class LackOfCohesionRefactoring : IRefactoring
+    {
+        public string DiagnosticId => "SASKIA112";
+
+        public string Title => DiagnosticId;
+
+        public string Description => Title;
+
+        public IEnumerable<SyntaxNode> ApplyFix(SyntaxNode node) =>
+            new [] { node };
+    
+        public DiagnosticInfo DoDiagnosis(SyntaxNode node)
+        {
+            const double lackOfCohesionThreshold = 0.6d;
+
+            var classNode = (ClassDeclarationSyntax) node;
+            var fieldNodeList = classNode.Members.OfType<FieldDeclarationSyntax>().ToList();
+            var methodNodeList = classNode.Members.OfType<MethodDeclarationSyntax>().ToList();
+            var fieldAccessCounterMap = fieldNodeList.ToDictionary(field => field, _ => 0);
+            
+            foreach (var method in methodNodeList)
+            {
+                var visitedFields = new HashSet<FieldDeclarationSyntax>();
+                GetMethodsFieldAccesses(method, fieldNodeList, visitedFields);
+
+                foreach (var visitedField in visitedFields)
+                {
+                    ++fieldAccessCounterMap[visitedField];
+                }
+            }
+
+            var averageFieldAccessCount = fieldAccessCounterMap.Values.Sum() / (double) fieldNodeList.Count;
+            var numberOfMethods = methodNodeList.Count;
+
+            var lackOfCohesionValue = numberOfMethods < 2 ? 0d : (numberOfMethods - averageFieldAccessCount) / 
+                                                 (numberOfMethods - 1);
+
+            return lackOfCohesionValue > lackOfCohesionThreshold ? 
+                DiagnosticInfo.CreateFailedResult("LCOM!", lackOfCohesionValue) :
+                DiagnosticInfo.CreateSuccessfulResult(lackOfCohesionValue);
+        }
+
+        private static void GetMethodsFieldAccesses(SyntaxNode node, 
+            IReadOnlyCollection<FieldDeclarationSyntax> availableFields,
+            ISet<FieldDeclarationSyntax> visitedMap)
+        {
+            if (node is IdentifierNameSyntax identifierNode)
+            {
+                var accessedFieldName = identifierNode.Identifier.Text;
+
+                var accessedField = availableFields
+                    .FirstOrDefault(field => field.Declaration.Variables[0].Identifier.Value ==
+                    accessedFieldName);
+
+                if (accessedField != null)
+                    visitedMap.Add(accessedField);
+            }
+
+            foreach (var child in node.ChildNodes())
+            {
+                GetMethodsFieldAccesses(child, availableFields, visitedMap);
+            }
+        }
+
+        public SyntaxNode GetReplaceableNode(SyntaxToken token) =>
+            SyntaxNodeHelper.FindAncestorOfType<ClassDeclarationSyntax>(token);
+
+        public IEnumerable<SyntaxKind> GetSyntaxKindsToRecognize() =>
+            new[] {SyntaxKind.ClassDeclaration};
+    }
+}
---
+++ RefactoringTesting/LackOfCohesionRefactoringTesting.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Refactoring.Refactorings.LackOfCohesion;
+
+namespace RefactoringTesting
+{
+    [TestClass]
+    public sealed class LackOfCohesionRefactoringTesting
+    {
+        [TestMethod]
+        public void OneMethodOneFieldTest()
+        {
+            CheckLackOfCohesion("class A { int x; public void X() { x = 12; } }", false, 0);
+        }
+
+        [TestMethod]
+        public void NoMethodTest()
+        {
+            CheckLackOfCohesion("class A { int x; int y; }", false, 0);
+        }
+
+        [TestMethod]
+        public void SimpleLackOfCohesionTest()
+        {
+            CheckLackOfCohesion("class A { int x; public int X() { return x; }" +
+                                "int y; public int Y() { return y; }" +
+                                "}", true, 1.0d);
+        }
+
+        [TestMethod]
+        public void NoLackOfCohesionTest()
+        {
+            CheckLackOfCohesion("class A { int x; int y; int z; public void X() { x = 12; y = 12; }" +
+                                "public void Y() { y = 12; z = 4; } }", true, 2d / 3d);
+        }
+
+        [TestMethod]
+        public void GetterSetterTest()
+        {
+            CheckLackOfCohesion("class X { int x; int y; public int X => x; public int Y => y; }",
+                false, 0);
+        }
+
+        [TestMethod]
+        public void EditorSample1Test()
+        {
+            CheckLackOfCohesion(@"public class X
+                {
+                    private int x;
+                    private int a;
+
+                    public void A()
+                    {
+                        Console.WriteLine(x + a);
+                    }
+
+                    public void B()
+                    {
+                        x = 12;
+                        a = 4;
+                    }
+                }", false, 0d);
+        }
+
+        private static void CheckLackOfCohesion(string source, bool diagnosticFound, double lackOfCohesionValue)
+        {
+            var refactoring = new LackOfCohesionRefactoring();
+            var classNode = GetClassNode(Compile(source));
+            var result = refactoring.DoDiagnosis(classNode);
+            Assert.AreEqual(diagnosticFound, result.DiagnosticFound);
+            Assert.IsTrue(Math.Abs(lackOfCohesionValue - (double) result.AdditionalInformation) < 0.01f);
+        }
+
+        private static SyntaxNode GetClassNode(SyntaxNode node)
+        {
+            if (node is ClassDeclarationSyntax)
+                return node;
+
+            return node.ChildNodes()
+                .Select(GetClassNode)
+                .FirstOrDefault(classNode => classNode != null);
+        }
+
+        private static SyntaxNode Compile(string source)
+        {
+            return CSharpSyntaxTree.ParseText(source)
+                .GetRoot();
+        }
+    }
+}
